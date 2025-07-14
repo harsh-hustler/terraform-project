2c05ffae3486b309ef5451f9b0d17d7076138752
harsh-hustler <78041915+harsh-hustler@users.noreply.github.com>
2025-07-15 00:10:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,9 +4,9 @@ region = "ap-southeast-1"
 }
 
 resource "aws_instance" "one" {
-  ami             = "ami-06018068a18569ff2"
+  ami             = "ami-0e7f9c9fced6cfb10"
   instance_type   = "t2.micro"
-  key_name        = "rahamabc"
+  key_name        = "Harsh"
   vpc_security_group_ids = [aws_security_group.five.id]
   availability_zone = "ap-southeast-1a"
   user_data       = <<EOF
@@ -23,9 +23,9 @@ EOF
 }
 
 resource "aws_instance" "two" {
-  ami             = "ami-06018068a18569ff2"
+  ami             = "ami-0e7f9c9fced6cfb10"
   instance_type   = "t2.micro"
-  key_name        = "rahamabc"
+  key_name        = "Harsh"
   vpc_security_group_ids = [aws_security_group.five.id]
   availability_zone = "ap-southeast-1b"
   user_data       = <<EOF
@@ -42,9 +42,9 @@ EOF
 }
 
 resource "aws_instance" "three" {
-  ami             = "ami-06018068a18569ff2"
+  ami             = "ami-0e7f9c9fced6cfb10"
   instance_type   = "t2.micro"
-  key_name        = "rahamabc"
+  key_name        = "Harsh"
   vpc_security_group_ids = [aws_security_group.five.id]
   availability_zone = "ap-southeast-1a"
   tags = {
@@ -53,9 +53,9 @@ resource "aws_instance" "three" {
 }
 
 resource "aws_instance" "four" {
-  ami             = "ami-06018068a18569ff2"
+  ami             = "ami-0e7f9c9fced6cfb10"
   instance_type   = "t2.micro"
-  key_name        = "rahamabc"
+  key_name        = "Harsh"
   vpc_security_group_ids = [aws_security_group.five.id]
   availability_zone = "ap-southeast-1b"
   tags = {
@@ -88,7 +88,7 @@ resource "aws_security_group" "five" {
 }
 
 resource "aws_s3_bucket" "six" {
-  bucket = "devopsbyrahamterraserverbucket9988oo9988"
+  bucket = "devopsbyrahamterraserverbucket9988oo9988harsh"
 }
 
 resource "aws_iam_user" "seven" {
